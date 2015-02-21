b17e525ca2eb010ce95b7252299d55c05ae45d3e
0xADADA <0xadada.pub@0xadada.pub>
2015-02-20 21:08:10 -0500

    Streamlined provisioning script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,35 +1,34 @@
 #!/usr/bin/env bash
 cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
-function doIt() {
-    rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-        --exclude "README.md" --exclude "LICENSE-MIT.txt" \
-        --exclude "Brewfile" --exclude ".cask" --exclude ".osx" \
+function init_home() {
+    rsync --exclude ".git/" \
+        --exclude ".DS_Store" \
+        --exclude "bootstrap.sh" \
+        --exclude "provision.sh" \
+        --exclude "README.md" \
+        --exclude "LICENSE-MIT.txt" \
+        --exclude "Brewfile" \
+        --exclude "Caskfile" \
+        --exclude ".osx" \
         -av --no-perms . ~
     source ~/.bash_profile
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
-    doIt
+    echo "Initializing home directory dotfiles"
+    init_home
 else
     read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
     echo
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        doIt
+        echo "Initializing home directory dotfiles"
+        init_home
     fi
 fi
-unset doIt
+unset init_home
 
-# update homebrew packages
-if [ `type -P brew` ]; then
-    echo "Installing Homebrew packages..."
-    brew bundle Brewfile
-    [[ $? ]] && echo "Done"
-else
-    printf "\n"
-    echo "\nError: brew not found."
-    printf "Install at http://brew.sh/\n"
-    exit
-fi
+# call homebrew and homebrew cask scripts
+source provision.sh
 
 # update / install npm packages
 # Check for npm
@@ -37,7 +36,7 @@ if [ `type -P npm` ]; then
     echo "Installing Node.js packages..."
 
     # List of npm packages
-    packages="bower grunt-cli jshint yo"
+    packages="bower grunt-cli jshint"
 
     # Install packages globally and quietly
     npm install $packages --global --quiet
@@ -46,6 +45,6 @@ if [ `type -P npm` ]; then
 else
     printf "\n"
     echo "Error: npm not found."
-    printf "Aborting...\n"
+    printf "Aborting... try installing node packages manually\n"
     exit
 fi
