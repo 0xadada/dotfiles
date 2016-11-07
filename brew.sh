e422014877ac10d71c83abe10d36500b02a739f6
0xADADA <0xadada.pub@0xadada.pub>
2016-11-07 17:41:27 -0500

    Added docker for mac

--- brew.sh
+++ brew.sh
@@ -45,6 +45,7 @@ fi
 brew cask install bitcoin-core
 brew cask install bittorrent-sync
 brew cask install diffmerge
+brew cask install docker
 brew cask install google-earth
 brew cask install firefox
 brew cask install flux
