e00ec697b384ac48ac92756aab78515a580f7d58
0xADADA <0xadada.pub@0xadada.pub>
2020-03-21 11:29:24 -0400

    throttle timing on packetloss when no network

--- bin/notify-on-packetloss.sh
+++ bin/notify-on-packetloss.sh
@@ -2,6 +2,8 @@
 # Send a macos notification when packetloss is detected
 # Author @0xADADA
 
+trap '' EXIT
+
 GATEWAY=$(netstat -rn | grep 'default' | head -n 1 | awk '{print $2}')
 # GATEWAY="10.255.244.19"
 COUNT_PACKETS=0
@@ -9,16 +11,24 @@ COUNT_DROPPED=0
 FAIL_PER=0
 
 while true; do
-  DATE=$(date +'%H:%M:%S.%s')
+  DATE=$(date +'%Y-%m-%d %H:%M:%S')
   COUNT_PACKETS=$(($COUNT_PACKETS + 1))
-  ping -c 1 -W 150 -q $GATEWAY &>/dev/null
-  if (( $? != 0 )); then
-    COUNT_DROPPED=$(($COUNT_DROPPED + 1))
-    FAIL_PER=$(echo $(bc <<< "scale=2; $COUNT_DROPPED / $COUNT_PACKETS * 100"))
-    MESSAGE="Dropped $COUNT_DROPPED of $COUNT_PACKETS packets: $FAIL_PER%"
-    echo $MESSAGE
-    SCRIPT="$(echo display notification \"$MESSAGE\" with title \"Packetloss\")"
-    echo \'"$SCRIPT"\' | xargs osascript -e
+  NETWORK_STATE=$(ifconfig en0 | rg 'status:' | awk '{print $2}') # active|inactive
+  # run only if the network interface is active
+  if [[ "${NETWORK_STATE}" == "active" ]]; then
+    PING_MS=$(ping -c 1 -W 500 -q $GATEWAY | rg 'round-trip' | awk '{print $4}')
+    echo "${DATE} response: ${PING_MS}"
+    if (( $? != 0 )); then
+      COUNT_DROPPED=$(($COUNT_DROPPED + 1))
+      FAIL_PER=$(echo $(bc <<< "scale=2; $COUNT_DROPPED / $COUNT_PACKETS * 100"))
+      MESSAGE="Dropped $COUNT_DROPPED of $COUNT_PACKETS packets: $FAIL_PER%"
+      echo $MESSAGE
+      SCRIPT="$(echo display notification \"$MESSAGE\" with title \"Packetloss\")"
+      echo \'"$SCRIPT"\' | xargs osascript -e
+    fi
+    sleep 1
+  else
+    echo "${DATE} network inactive"
+    sleep 10
   fi
-  sleep 1
 done
