006f2af42d04505bf18f6468488f3122bcad91bd
0xADADA <0xadada.pub@0xadada.pub>
2020-03-21 22:59:05 -0400

    allow logging to stderr

--- bin/notify-on-packetloss.sh
+++ bin/notify-on-packetloss.sh
@@ -11,14 +11,14 @@ fail_per=0
 while true; do
   datetime=$(date +'%Y-%m-%d %H:%M:%S')
   count_packets=$(($count_packets + 1))
-  network_state=$(ifconfig en0 | rg 'status:' | awk '{print $2}') # active|inactive
+  network_state="$(ifconfig en0 | grep 'status:' | awk '{print $2}')" # active|inactive
   # run only if the network interface is active
   if [[ "${network_state}" == "active" ]]; then
     gateway=$(netstat -rn | grep 'default' | head -n 1 | awk '{print $2}')
     ping="$(ping -c 1 -W 500 -q ${gateway} 2>&1)"
     result=$?
-    packet_loss=$(echo "${ping}" | rg 'packet loss' | awk '{print $7}')
-    # echo "${datetime} packet ${count_packets} status   ${packet_loss}"
+    packet_loss=$(echo "${ping}" | grep 'packet loss' | awk '{print $7}')
+    echo "${datetime} packet ${count_packets} status ${packet_loss}"
     if (( $result != 0 )); then
       count_dropped=$(($count_dropped + 1))
       fail_per=$(echo $(bc <<< "scale=2; ${count_dropped} / ${count_packets} * 100"))
