6f8e0b9f95c165e328bdfc3853d72106b199abe8
0xADADA <0xadada.pub@0xadada.pub>
2020-03-22 09:29:26 -0400

    implemented installer for notify-on-packetloss

---
+++ bin/install-notify-on-packetloss.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# Installs notify-on-packetloss.sh
+# Author @0xADADA
+
+cp \
+  "${HOME}/bin/pub.0xadada.notify-on-packetloss.plist" \
+  "${HOME}/Library/LaunchAgents/pub.0xadada.notify-on-packetloss.plist"
+
+# edit path in the ProgramArguments value
+sed -i -e "s/USER/$USER/g" \
+  ~/Library/LaunchAgents/pub.0xadada.notify-on-packetloss.plist
+
+# install it
+launchctl remove pub.0xadada.notify-on-packetloss
+launchctl load \
+  "/Users/${USER}/Library/LaunchAgents/pub.0xadada.notify-on-packetloss.plist"
+result=$?
+
+if (( $result != 0 )); then
+  echo "Failed to install with code: ${result}"
+else
+  pid=$(launchctl list | rg 'pub.0xadada.notify-on-packetloss' | awk '{print $1}')
+  echo "Successfully installed notify-on-packetloss.sh, PID ${pid}"
+fi
