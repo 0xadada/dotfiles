89aa1c5fd795f55309caca6b993107fc81cfbc96
0xADADA <0xadada.pub@0xadada.pub>
2023-11-14 13:28:55 -0500

    linting

--- bootstrap.sh
+++ bootstrap.sh
@@ -87,7 +87,7 @@ if [[ "${REPLY}" =~ ^[Yy]$ ]]; then
   # shellcheck disable=SC2164
   cd "$NVM_DIR"
   git checkout "$(git describe --abbrev=0 --tags --match "v[0-9]*" "$(git rev-list --tags --max-count=1)")"
-  # shellcheck disable=SC1091
+  # shellcheck disable=SC1090,SC1091
   \. "$NVM_DIR/nvm.sh"
   nvm install --lts
 fi
