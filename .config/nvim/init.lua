e220a7b04dde601e28a10456971910352213bd83
0xADADA <0xadada.pub@0xadada.pub>
2024-02-07 22:22:21 -0500

    converted config to Lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,6 @@
-local vimrc = vim.fn.stdpath("config") .. "/init.legacy.vim"
-vim.cmd.source(vimrc)
+local vim = vim
 
 -- boot Plug
-local vim = vim
 local Plug = vim.fn['plug#']
 vim.call('plug#begin')
 -- load Plugs
@@ -31,9 +29,75 @@ Plug('vim-pandoc/vim-pandoc-syntax')
 Plug('prettier/vim-prettier', { ['ft'] = {'javascript', 'typescript', 'html', 'css', 'scss', 'json', 'graphql', 'markdown', 'yaml'} })
 vim.call('plug#end')
 
+-- vim options
+vim.opt.ruler = true -- show the cursor position
+vim.opt.selection = "exclusive" -- visual mode selection to exlusive (rather than inclusive)
+vim.opt.clipboard = "unnamed" -- Use the OS clipboard by default (on versions compiled with `+clipboard`)
+vim.opt.wildmenu = true -- Enhance command-line completion
+vim.opt.backspace = "indent,eol,start" -- Allow backspace in insert mode
+vim.opt.ttyfast = true -- optimize for fast terminal connections
+vim.opt.gdefault = true -- Add the g flag to search/replace by default
+vim.opt.encoding = "utf-8" -- use UTF-8
+vim.opt.binary = true -- Don’t add empty newlines at the end of files
+vim.opt.modeline = true -- Respect modeline in files
+vim.opt.modelines = 4
+vim.opt.exrc = true -- enable per-directory .vimrc files and disable unsafe commands in them
+vim.opt.secure = true
+vim.opt.number = true -- enable line numbers
+vim.opt.tabstop = 4 -- tabs as wide as four spaces
+vim.opt.shiftwidth = 4 -- make shift indent operation add four spaces
+vim.opt.expandtab = true -- convert tabs to spaces upon tabpress
+vim.opt.list = true
+vim.opt.hlsearch = true -- highlight searches
+vim.opt.incsearch = true -- highlight dynamically as pattern is typed
+vim.opt.laststatus = 2 -- always show status line
+vim.opt.mouse = "a" -- enable mouse in all modes
+vim.opt.shortmess = "atI" -- don’t show the intro message when starting vim
+vim.opt.showmode = true -- show the current mode
+vim.opt.title = true -- show the filename in the window titlebar
+vim.opt.showcmd = true -- show the (partial) command as it’s being typed
+vim.opt.scrolloff = 4 -- start scrolling three lines before the horizontal window border
+vim.opt.foldmethod = "indent" -- code folding
+vim.opt.foldnestmax = 10
+vim.opt.foldlevel = 2
+
+vim.g.mapleader = "," -- Change mapleader from "\" (default) to comma
+
+vim.cmd.syntax("on") -- enable syntax highlighting
+vim.cmd.filetype("plugin indent on") --enable plugins
+-- legacy shit
+vim.cmd [[ let &colorcolumn=join(range(120,666),",") ]] -- highlight at column 121
+vim.cmd [[setlocal noeol nofoldenable nocursorline noerrorbells nostartofline]]
+vim.cmd("set lcs+=leadmultispace:∙,tab:_,trail:∙,eol:↲")
+vim.cmd [[setlocal lcs=tab:▸\ ,trail:·,eol:¬,nbsp:_]]
+
+-- vim-airline
+vim.opt.guifont = "Source Code Pro for Powerline:h12"
+vim.g["airline_detect_paste"] = 1 -- enable paste detection
+vim.g["airline_powerline_fonts"] = 1
+vim.g["airline#extensions#branch#enabled"] = 1 -- vim-airline enable branch "fugitive" extension
+vim.g["airline#extensions#branch#empty_message"] = ''
+vim.g["airline#extensions#branch#displayed_head_limit"] = 10
+
 -- Gruvbox color palette
 vim.o.background = "dark"
 vim.cmd.colorscheme("gruvbox")
+
+-- HerringtonDarkholme/yats.vim
+vim.g["yats_host_keyword"] = 1 -- yats should handle specfic keywords
+
+-- MaxMEllon/vim-jsx-pretty
+vim.g["vim_jsx_pretty_disable_tsx"] = 1 -- let yats handle .tsx
+
+-- NERDTree
+vim.g["NERDTreeIgnore"] = { "^dist$", "^node_modules$" }
+
+-- vim-mix-format set to run Elixir formatter upon save
+vim.g["mix_format_on_save"] = 1
+
+-- vim/prettier setup
+vim.g['prettier#autoformat'] = 1
+vim.g['prettier#autoformat_require_pragma'] = 0
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -129,10 +193,6 @@ cmp.setup {
     { name = 'luasnip' },
   },
 }
-
--- vim/prettier setup
-vim.g['prettier#autoformat'] = 1
-vim.g['prettier#autoformat_require_pragma'] = 0
 
 local lsp_flags = {
   -- This is the default in Nvim 0.7+
